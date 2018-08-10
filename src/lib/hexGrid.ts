17be8147d6174232216cf694cbdbe3c8af9e7fee
Brad Anderson <banderso@nerdery.com>
2018-08-09 21:02:09 -0500

    Generates edges from hex grid (but doesn't dedupe them yet).

--- src/lib/hexGrid.ts
+++ src/lib/hexGrid.ts
@@ -14,16 +14,17 @@ export const parseHexGrid = (tiles: Vector2[], r: number): HexMapParseResult => 
   const hexHeight = 2 * r;
   const vertices: Vector2[] = [];
   const edges: Vector2[] = [];
-  tiles.forEach(([ col, row ]) => {
+  tiles.forEach(([ col, row ], i) => {
     const tilePosition: Vector2 = [
       col * hexWidth + (row % 2 * 0.5 * hexWidth),
       row * 0.75 * hexHeight,
     ];
-    times(6, i => {
-      const theta = ONE_SIXTH_TAU * i;
+    times(6, j => {
+      const theta = ONE_SIXTH_TAU * j;
       const x = round(tilePosition[0] + r * Math.sin(theta), 5);
       const y = round(tilePosition[1] + r * Math.cos(theta), 5);
-      vertices.push([ x, y ]);
+      const newLength = vertices.push([ x, y ]);
+      edges.push([ newLength - 1, newLength % 6 + i * 6 ]);
     });
   });
 
